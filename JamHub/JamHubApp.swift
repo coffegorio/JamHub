358bdba0cfa4df7aece981885c5eb0a2c97c2c79
coffegorio <coffegorio@vk.com>
2024-11-02 04:27:11 +0300

    Initial Commit

---
+++ JamHub/JamHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  JamHubApp.swift
+//  JamHub
+//
+//  Created by Егорио on 02.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct JamHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
