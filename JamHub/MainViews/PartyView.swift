99c89d8550d6cbf8b3c7d2032a744f58b685af48
coffegorio <coffegorio@vk.com>
2024-11-05 05:23:30 +0300

    beggining of mainpage + tabbar

---
+++ JamHub/MainViews/PartyView.swift
@@ -0,0 +1,18 @@
+//
+//  PartyView.swift
+//  JamHub
+//
+//  Created by Егорио on 05.11.2024.
+//
+
+import SwiftUI
+
+struct PartyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PartyView()
+}
